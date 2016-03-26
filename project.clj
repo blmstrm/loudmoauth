09631684fefaba5ed9d1bea7ab6ff67d29bbf88c
Kalle Blomström <kalle.blomstrom@gmail.com>
2016-03-26 09:44:32 +0100

    Adds cloverage plugin

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/blmstrm/loudmoauth"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
+  :plugins [[lein-cloverage "1.0.6"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.clojure/core.async "0.2.374"]
