15736c67cad35e11bd83cee4c2ca43217f3269d9
Kalle Blomström <kalle.blomstrom@gmail.com>
2016-06-14 15:13:15 +0200

    Bumps version numbers for core.async, clj-http and schema.

--- project.clj
+++ project.clj
@@ -6,9 +6,8 @@
   :plugins [[lein-cloverage "1.0.6"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.2.382"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/algo.generic "0.1.2"]
-                 [clj-http "2.1.0"]
-                 [prismatic/schema "1.1.0"]
-                 ])
+                 [clj-http "3.1.0"]
+                 [prismatic/schema "1.1.2"]])
